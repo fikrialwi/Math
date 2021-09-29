e7f8fd368d2ee16f7f3ed22556c07713695da493
fikrialwi <73596865+fikrialwi@users.noreply.github.com>
2021-09-29 10:15:52 +0700

    Add files via upload
    menambah file encrypt caesar chipper -pelajaran kriptografi

---
+++ caesarchiper.py
@@ -0,0 +1,18 @@
+def encryptCC(txt,n):
+    p = ""
+    for i in txt:
+        if i == " ":
+            p+=" "
+        p += chr((ord(i)-97+n)%26+97)
+    return p    
+def decryptCC(txt,n):
+    p = ""
+    for i in txt:
+        if i == " ":
+            p+=" "
+        p += chr((ord(i)-97-n)%26+97) 
+    return p
+print(encryptCC("halo nama saya dzulfikri",6))
+for i in range(0,27):
+    # TODO: write code...
+    print(decryptCC("ymnmdbgnswgk",i),"-",i)
