d780f1b53519ee0b333d2bc6b5745aff27d2bd6a
Dzulfikri Alwi Muhammad <73596865+fikrialwi@users.noreply.github.com>
2022-01-10 15:04:28 +0700

    menambah fungsional koding

--- graf/grafJacobson/run.py
+++ graf/grafJacobson/run.py
@@ -1,54 +1,68 @@
 import Bilangan as bil
-import Graf as graf
+import Graf as gf
+import networkx as nx
+import matplotlib.pyplot as m
 
-def run(type = 'default'):
-    if type == 'default':
-        ring = input("masukkan ring : ")
+class show():
+    def __init__(self):
+        self.graph = input('masukkan jenis graf : ')
+        self.run()
+    g={
+        'jacobson':gf.GrafJacobson,
+        'annihilator':gf.GrafAnnihilator,
+        'unit':gf.GrafUnit,
+        'zero divisor':gf.GrafZeroDivisor,
+        'total zero divisor':gf.GrafTotalZeroDivisor,
+        'total':gf.GrafTotal
+    }
+    
+    
+    def run(self):
+        number = input("masukkan angka : ").split(',')
         operation = input("masukkan operation : ")
+        if len(number) == 1:
+            self.temp(self.g[self.graph],number[0],operation)
+            ring = 'Z_'+number[0]
+            G = nx.Graph()
+            graf = self.g[self.graph](ring,operation)
+            G.add_nodes_from(graf.vertex())
+            G.add_edges_from(graf.edge())
+            nx.draw_circular(G, with_labels = True, node_color='r', edge_color='b', font_weight='300', font_color='w', font_size='small')
 
-        print(f'Ring & operation anda : ({ring}, {operation})')
-        jacobs = graf.GrafJacobson(ring,operation)
-        modulo = bil.Modulo(jacobs.getNumber(),operation)
+        else:
+            for i,e in enumerate(number):
+                self.temp(self.g[self.graph],e,operation)
+                ring = 'Z_'+e
+                G = nx.Graph()
+                graf = self.g[self.graph](ring,operation)
+                G.add_nodes_from(graf.vertex())
+                G.add_edges_from(graf.edge())
+                nx.draw_circular(G, with_labels = True, node_color='r', edge_color='b', font_weight='300', font_color='w', font_size='small')
+                ax = m.subplot(len(number)//2,3,i+1)
+        m.show()
+    def temp(self,graph, number, operation):
+        print(f'Ring & operation anda : (Z_{number}, {operation})')
+        ring = 'Z_'+number
+        graf = graph(ring,operation)
+        modulo = bil.Modulo(graf.getNumber(),operation)
 
         print('-'*10 +'start' + '-'*10)
         print('')
-        print(f'element dari {ring} :\n{modulo.element()}')
+        print(f'element dari {number} :\n{modulo.element()}')
         print('-'*25)
-        print(f'element unit dari {ring} :\n{modulo.unit()}')
+        print(f'simpul graf {self.get_key(graph)} pada ring Z_{number}: \n{graf.vertex()}')
         print('-'*25)
-        print(f'ideal maksimal dari {ring} :\n{modulo.idealMax()}')
+        print(f'sisi graf {self.get_key(graph)} pada ring Z_{number}: \n{graf.edge()}')
         print('-'*25)
-        print(f'jacobson dari {ring} :\n{jacobs.get_jacobson()[0]}')
+        print(f'orde dari graf {self.get_key(graph)} pada ring Z_{number}: \n{graf.orde()}')
         print('-'*25)
-        print(f'simpul graf jacobson pada ring {ring}: \n{jacobs.vertex()}')
-        print('-'*25)
-        print(f'sisi graf jacobson pada ring {ring}: \n{jacobs.edge()}')
+        print(f'size dari graf {self.get_key(graph)} pada ring Z_{number}: \n{graf.size()}')
         print('-'*25)
         print('')
         print('-'*10+'end' + '-'*10)
-    elif type == 'multiple':
-        data = input('masukkan data: ').split(',')
-        operation = input("masukkan operation : ")
-
-        print('-'*10 +'start' + '-'*10)
-        print('')
-        for i in data:
-            ring = 'Z_'+i
-            print(f'Ring & operation anda : ({ring}, {operation})')
-            jacobs = graf.GrafJacobson(ring,operation)
-            modulo = bil.Modulo(jacobs.getNumber(),operation)
-            print(f'element dari {ring} :\n{modulo.element()}')
-            print('-'*25)
-            print(f'element unit dari {ring} :\n{modulo.unit()}')
-            print('-'*25)
-            print(f'ideal maksimal dari {ring} :\n{modulo.idealMax()}')
-            print('-'*25)
-            print(f'jacobson dari {ring} :\n{jacobs.get_jacobson()}')
-            print('-'*25)
-            print(f'simpul graf jacobson pada ring {ring}: \n{jacobs.vertex()}')
-            print('-'*25)
-            print(f'sisi graf jacobson pada ring {ring}: \n{jacobs.edge()}')
-            print('-'*25)
-        print('')
-        print('-'*10+'end' + '-'*10)
-run(type='multiple')
+    def get_key(self,val):
+        for key, value in self.g.items():
+            if val == value:
+                return key
+        return "key doesn't exist"
+show()
