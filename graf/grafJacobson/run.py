af955e3fcdf42db1a5914939bb533faf42824ac8
Dzulfikri Alwi Muhammad <73596865+fikrialwi@users.noreply.github.com>
2022-01-16 05:58:58 +0700

    optimalisasi run

--- graf/grafJacobson/run.py
+++ graf/grafJacobson/run.py
@@ -2,10 +2,11 @@ import Bilangan as bil
 import Graf as gf
 import networkx as nx
 import matplotlib.pyplot as m
+import TopologiIndex as ti
 
 class show():
     def __init__(self):
-        self.graph = input('masukkan jenis graf : ')
+        self.graph = input('masukkan jenis graf : ').lower()
         self.run()
     g={
         'jacobson':gf.GrafJacobson,
@@ -13,34 +14,49 @@ class show():
         'unit':gf.GrafUnit,
         'zero divisor':gf.GrafZeroDivisor,
         'total zero divisor':gf.GrafTotalZeroDivisor,
-        'total':gf.GrafTotal
+        'total':gf.GrafTotal,
+        'identity':gf.GrafIdentity,
+        'maximal': gf.GrafMaximal,
     }
-    
+    t = {
+        'first zagreb': ti.FirstZagreb,
+        'second zagreb' : ti.SecondZagreb,
+        'first multiple zagreb': ti.FirstMultipleZagreb,
+        'second multiple zagreb': ti.SecondMultipleZagreb,
+        'narumi katayama': ti.NarumiKatayama,
+        'radic': ti.Randic,
+        'forgotten': ti.ForgottenTopological,
+        'reciprocal radic': ti.ReciprocalRandic,
+        'reduce reciprocal radic': ti.ReducedReciprocalRandic,
+        'first zagreb coindex': ti.FirstZagrebCoindex,
+        'second zagreb coindex': ti.SecondZagrebCoindex,
+        'reduced second zagreb': ti.ReducedSecondZagreb,
+        'abc': ti.AtomBondConectivity,
+        'harmonic': ti.Harmonic,
+        '4th abc': ti.FouthVersionABC,
+        'augmented zagreb': ti.AugmentedZagreb,
+        'geometric aritmethic': ti.GeometricAritmethic,
+        '5th ga': ti.FifthVersionGA,
+        'sum connectivity': ti.SumConnectivity
+    }
     
     def run(self):
         number = input("masukkan angka : ").split(',')
         operation = input("masukkan operation : ")
-        if len(number) == 1:
-            self.temp(self.g[self.graph],number[0],operation)
-            ring = 'Z_'+number[0]
+        topologi = input('masukkan topologi indeks : ').lower()
+        for i,e in enumerate(number):
+            self.temp_graph(self.g[self.graph],e,operation)
+            ring = 'Z_'+e
             G = nx.Graph()
             graf = self.g[self.graph](ring,operation)
             G.add_nodes_from(graf.vertex())
             G.add_edges_from(graf.edge())
             nx.draw_circular(G, with_labels = True, node_color='r', edge_color='b', font_weight='300', font_color='w', font_size='small')
-
-        else:
-            for i,e in enumerate(number):
-                self.temp(self.g[self.graph],e,operation)
-                ring = 'Z_'+e
-                G = nx.Graph()
-                graf = self.g[self.graph](ring,operation)
-                G.add_nodes_from(graf.vertex())
-                G.add_edges_from(graf.edge())
-                nx.draw_circular(G, with_labels = True, node_color='r', edge_color='b', font_weight='300', font_color='w', font_size='small')
-                ax = m.subplot(len(number)//2,3,i+1)
-        m.show()
-    def temp(self,graph, number, operation):
+            if topologi != False:
+                self.temp_topologi(graf,topologi)
+            m.savefig(f'hasil/img/graf {self.graph} pada {ring}')
+            self.create(ring,operation,self.graph,topologi)
+    def temp_graph(self,graph, number, operation):
         print(f'Ring & operation anda : (Z_{number}, {operation})')
         ring = 'Z_'+number
         graf = graph(ring,operation)
@@ -50,19 +66,27 @@ class show():
         print('')
         print(f'element dari {number} :\n{modulo.element()}')
         print('-'*25)
-        print(f'simpul graf {self.get_key(graph)} pada ring Z_{number}: \n{graf.vertex()}')
+        print(f'simpul graf {self.get_key(graph,self.g)} pada ring Z_{number}: \n{graf.vertex()}')
         print('-'*25)
-        print(f'sisi graf {self.get_key(graph)} pada ring Z_{number}: \n{graf.edge()}')
+        print(f'sisi graf {self.get_key(graph,self.g)} pada ring Z_{number}: \n{graf.edge()}')
         print('-'*25)
-        print(f'orde dari graf {self.get_key(graph)} pada ring Z_{number}: \n{graf.orde()}')
+        print(f'orde dari graf {self.get_key(graph,self.g)} pada ring Z_{number}: \n{graf.orde()}')
         print('-'*25)
-        print(f'size dari graf {self.get_key(graph)} pada ring Z_{number}: \n{graf.size()}')
+        print(f'size dari graf {self.get_key(graph,self.g)} pada ring Z_{number}: \n{graf.size()}')
         print('-'*25)
         print('')
         print('-'*10+'end' + '-'*10)
-    def get_key(self,val):
-        for key, value in self.g.items():
+    def get_key(self,val,arr):
+        for key, value in arr.items():
             if val == value:
                 return key
         return "key doesn't exist"
+    def temp_topologi(self, graph, topologi):
+        print(f'indeks {topologi}nya adalah {self.t[topologi](graph).topologi()}')
+    def create(self, ring, operator, graph, topologi):
+        f = open(f"hasil/graf {graph} pada {ring} dan topologi {topologi}.txt", "w")
+        graf = self.g[graph](ring,operator)
+        topo = self.t[topologi](graf)
+        text = f'graf : {graph}\nring : {ring}\noperator : {operator}\n\tsimpul : {graf.vertex()}\n\tsisi : {graf.edge()}\n\torde : {graf.orde()}\n\tsize : {graf.size()}\n =============== \n\ttopologi indeks : {topologi} => nilainya : {topo.topologi()}'
+        f.write(text)
 show()
