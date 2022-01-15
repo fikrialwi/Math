ab5a1e54a2f5303c25f94ce9cf98e2d376ada89c
Dzulfikri Alwi Muhammad <73596865+fikrialwi@users.noreply.github.com>
2022-01-16 06:01:33 +0700

    menambah dan mengoptimalisasi graf

--- graf/grafJacobson/Graf.py
+++ graf/grafJacobson/Graf.py
@@ -30,7 +30,7 @@ class GrafJacobson(GrafofRing):
         return super().getOperation()
     def getNumber(self):
         return super().getNumber()
-    def getJacobson(self):
+    def getJacobson(self):  
         return bil.Jacobson(self.getNumber(),self.getOperation())
     def get_jacobson(self):
         return self.getJacobson().jacobson()
@@ -58,7 +58,7 @@ class GrafJacobson(GrafofRing):
     def size(self):
         return len(self.edge())
     def edgeofVertex(self,vertex):
-        if vertex not in self.vertex() : 
+        if vertex not in self.vertex(): 
             return "Bilangan tidak termasuk dalam himpunan titik graf"
         edge = []
         for i in self.vertex():
@@ -98,7 +98,7 @@ class GrafUnit(GrafofRing):
         edge = []
         for i in vertex:
             for j in vertex:
-                if i != j and i+j%self.getNumber() in self.getUnit():
+                if i != j and (i+j)%self.getNumber() in self.getUnit():
                     edge.append({i,j})
         edge = self.getRing().uniq(edge)
         return list(map(tuple,edge))
@@ -194,7 +194,7 @@ class GrafTotal(GrafofRing):
         edge = []
         for i in vertex:
             for j in vertex:
-                if i != j and i+j%self.getNumber() in self.getZeroDivisor():
+                if i != j and self.isEdgeof(i,j):
                     edge.append({i,j})
         edge = self.getRing().uniq(edge)
         return list(map(tuple,edge))
@@ -242,7 +242,7 @@ class GrafTotalZeroDivisor(GrafofRing):
         edge = []
         for i in vertex:
             for j in vertex:
-                if i != j and (i+j)%self.getNumber() in self.getZeroDivisor() and (i*j)%self.getNumber() == 0 :
+                if i != j and self.isEdgeof(i,j) :
                     edge.append({i,j})
         edge = self.getRing().uniq(edge)
         return list(map(tuple,edge))
@@ -274,7 +274,7 @@ class GrafTotalZeroDivisor(GrafofRing):
             matrix.append(row)
         return matrix
 
-class GrafUnit(GrafofRing):
+class GrafIdentity(GrafofRing):
     def __init__(self, ring, operation):
         super().__init__(ring, operation)
     def getNumber(self):
@@ -290,7 +290,7 @@ class GrafUnit(GrafofRing):
         edge = []
         for i in vertex:
             for j in vertex:
-                if i != j and (i*j)%self.getNumber() == 1:
+                if i != j and self.isEdgeof(i,j):
                     edge.append({i,j})
         edge = self.getRing().uniq(edge)
         return list(map(tuple,edge))
@@ -303,7 +303,7 @@ class GrafUnit(GrafofRing):
             return "Bilangan tidak termasuk dalam himpunan titik graf"
         edge = []
         for i in self.vertex():
-            if self.isEdgeof(i*vertex):
+            if self.isEdgeof(i,vertex):
                 edge.append(i)
         return edge
     def degEdgeofVertex(self, vertex):
@@ -339,7 +339,7 @@ class GrafAnnihilator(GrafofRing):
         for i in vertex:
             for j in vertex:
                 ring = self.getRing()
-                if i != j and self.getRing().union(ring.annihilator(i), ring.annihilator(j)) != ring.annihilator(i*j%self.getNumber()):
+                if i != j and self.isEdgeof(i,j):
                     edge.append({i,j})
         edge = self.getRing().uniq(edge)
         return list(map(tuple,edge))
@@ -352,8 +352,7 @@ class GrafAnnihilator(GrafofRing):
             return "Bilangan tidak termasuk dalam himpunan titik graf"
         edge = []
         for i in self.vertex():
-            ring = self.getRing()
-            if self.getRing().union(ring.annihilator(i), ring.annihilator(vertex)) != ring.annihilator(i*vertex%self.getNumber()):
+            if self.isEdgeof(i,vertex):
                 edge.append(i)
         return edge
     def degEdgeofVertex(self, vertex):
@@ -372,3 +371,55 @@ class GrafAnnihilator(GrafofRing):
                     row.append(0)
             matrix.append(row)
         return matrix
+
+class GrafMaximal(GrafofRing):
+    def __init__(self, ring, operation):
+        super().__init__(ring, operation)
+    def getNumber(self):
+        return super().getNumber()
+    def getRing(self):
+        return super().getRing()
+    def getIdealMax(self):
+        return self.getRing().idealMax()
+    def vertex(self):
+        return self.getRing().element()
+    def edge(self):
+        vertex = self.vertex()
+        edge = []
+        for i in vertex:
+            for j in vertex:
+                if i != j and self.isEdgeof(i,j):
+                    edge.append({i,j})
+        edge = self.getRing().uniq(edge)
+        return list(map(tuple,edge))
+    def orde(self):
+        return len(self.vertex())
+    def size(self):
+        return len(self.edge())
+    def edgeofVertex(self,vertex):
+        if vertex not in self.vertex() : 
+            return "Bilangan tidak termasuk dalam himpunan titik graf"
+        edge = []
+        for i in self.vertex():
+            if self.isEdgeof(i,vertex):
+                edge.append(i)
+        return edge
+    def degEdgeofVertex(self, vertex):
+        return len(self.edgeofVertex(vertex))
+    def isEdgeof(self,vertex1,vertex2):
+        for i in self.getIdealMax():
+            if vertex1 in i and vertex2 in i:
+                return True
+        return False
+    def matrixAdjencey(self):
+        matrix = []
+        for i in self.vertex():
+            row = []
+            for j in self.vertex():
+                if i != j and self.isEdgeof(i,j):
+                    row.append(1)
+                else:
+                    row.append(0)
+            matrix.append(row)
+        return matrix
+
